ae5af1508c4103ee22d92e7851e8c2abdbbd9cd3
Rudraksh Malhotra <rudraksh.malhotra.098@gmail.com>
2026-08-01 19:11:05 +0530

    feat: add comments

--- function&scope/this-bugs.js
+++ function&scope/this-bugs.js
@@ -2,28 +2,35 @@
 // BUG 1: Regular Method Losing this
 // -------------------------
 
+
 const taskBoard = {
     name: "ProjectFlow Board",
 
     showName() {
-        console.log(this.name);
+        console.log(this.name); 
     }
 };
 
+
 // Method reference is copied into a standalone variable
-const callback = taskBoard.showName;
+const callback = taskBoard.showName; //only saved function...not the object ->> no "this"
 
 try {
-    callback(); // 'this' is lost
+    callback(); // 'this' is lost as only function was saved...
 } catch (error) {
     console.log(error.message);
 }
 
 
 // Fix using bind()
-const fixedCallback = taskBoard.showName.bind(taskBoard);
+const fixedCallback = taskBoard.showName.bind(taskBoard); //binded object also with funciton.. ->> "this" works...
 
 fixedCallback();
+
+
+
+
+
 
 
 // -------------------------
